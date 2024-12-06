19b81cb03bd91ab5c1734539c961fd05f4a68ff9
Иван Горбунов <muervos@muervos.muervos>
2024-12-06 20:40:25 +0300

    first commit for mobile app

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Button, Alert, Image, TouchableWithoutFeedback } from 'react-native';
 
 export default function App() {
+  const handleButtonPress = () => Alert.alert("Уведомление", "вы нажали на кнопку", [
+    {text: "да", onPress:() => console.log("Yes")},
+    {text: "Отмена", onPress:() => console.log("No")}
+  ])
+  const handleTextPress = () => console.log('text pressed');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={2} style={styles.text} onPress={handleTextPress}>Hello{"\n"}Hello</Text>
+     
+      <Button title={'Нажми на меня'} color='black' onPress={handleButtonPress}/>
+      
+      <TouchableWithoutFeedback onPress={handleTextPress}>
+        <Image source={require('./assets/orig.png')} style={styles.img}/>
+      </TouchableWithoutFeedback>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +26,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    color: 'red',
+  },
+  img: {
+    width: 100,
+    height: 100,
   },
 });
